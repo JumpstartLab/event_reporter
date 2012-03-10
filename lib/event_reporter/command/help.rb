19328b75fe3caf82be05425f95fc0b6527377fdd
Mike Gehard <mike.gehard@livingsocial.com>
2012-03-10 08:17:52 -0700

    Add in ability to display help for different commands.

--- lib/event_reporter/command/help.rb
+++ lib/event_reporter/command/help.rb
@@ -4,13 +4,29 @@ module EventReporter
       # We take an unused argument here simply to implement the proper interface
       # for commands.
       def run(arguments)
-        <<-__
+        command_to_display_help_for = arguments.join(' ')
+        case command_to_display_help_for
+          when "find"
+            <<-__
+Load the queue with all records matching the criteria for the given attribute. Example usages:
+
+find zipcode 20011
+find last_name Johnson
+find state VA
+            __
+          when 'queue clear'
+            "Empty the queue.\n"
+          else
+            <<-__
 exit - End the program.
 help - Output a listing of the available individual commands.
+help <command> - Display help for an individual command. For example: help find or help queue clear
 load - Erase any loaded data and parse the specified file.
 find - Find records that match a certain search criteria.
 queue - Perform different actions on a queue loaded from a find command.
-        __
+            __
+        end
+
       end
     end
   end
--- spec/event_reporter/command/help_spec.rb
+++ spec/event_reporter/command/help_spec.rb
@@ -6,10 +6,27 @@ describe EventReporter::Command::Help do
     expected_output = <<-__
 exit - End the program.
 help - Output a listing of the available individual commands.
+help <command> - Display help for an individual command. For example: help find or help queue clear
 load - Erase any loaded data and parse the specified file.
 find - Find records that match a certain search criteria.
 queue - Perform different actions on a queue loaded from a find command.
 __
     EventReporter::Command::Help.new.run([]).should == expected_output
+  end
+
+  it "can list of help for a command that has one word" do
+    expected_output = <<-__
+Load the queue with all records matching the criteria for the given attribute. Example usages:
+
+find zipcode 20011
+find last_name Johnson
+find state VA
+    __
+    EventReporter::Command::Help.new.run(['find']).should == expected_output
+  end
+
+  it "can list of help for a command that has two words" do
+    expected_output = "Empty the queue.\n"
+    EventReporter::Command::Help.new.run(['queue', 'clear']).should == expected_output
   end
 end
