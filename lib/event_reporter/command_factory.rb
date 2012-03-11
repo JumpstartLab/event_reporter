eaae3931a1ee38ace7267f3c8b8ae5bcde18b3b8
Mike Gehard <mike.gehard@livingsocial.com>
2012-03-10 17:00:19 -0700

    Use some metaprogramming magic to instantiate the command classes. If you can't find a class then just return and unknown command.

--- lib/event_reporter/command_factory.rb
+++ lib/event_reporter/command_factory.rb
@@ -1,22 +1,13 @@
 # Load up all of the command files
-Dir[File.dirname(__FILE__) + '/command/*.rb'].map {|f| require f}
+Dir[File.dirname(__FILE__) + '/command/*.rb'].map { |f| require f }
 
 module EventReporter
   class CommandFactory
     def self.get(command)
-      case command
-        when 'exit'
-          EventReporter::Command::Exit.new
-        when 'help'
-          EventReporter::Command::Help.new
-        when 'find'
-          EventReporter::Command::Find.new
-        when 'load'
-          EventReporter::Command::Load.new
-        when 'queue'
-          EventReporter::Command::Queue.new
-        else
-          EventReporter::Command::Unknown.new
+      begin
+        EventReporter::Command.const_get(command.capitalize).new
+      rescue NameError
+        EventReporter::Command::Unknown.new
       end
     end
   end
